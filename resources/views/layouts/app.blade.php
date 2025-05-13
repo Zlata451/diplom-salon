044484de9b94e3bbeac3d11181ae4b1483994a79
Zlata <alwaysssmile981@gmail.com>
2025-05-13 18:06:44 +0200

    Виправлення для відображення стилей и скриптів

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-        <!-- Styles (заменили @vite на прямую ссылку на собранные ассеты) -->
+        <!-- Styles -->
         <link rel="stylesheet" href="{{ asset('build/assets/app-DXnCgaXg.css') }}">
         <script src="{{ asset('build/assets/app-Bo-u61x1.js') }}" defer></script>
     </head>
