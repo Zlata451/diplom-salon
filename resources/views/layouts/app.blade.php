5d765624172b2662b9348006ae05397f1eeeffe6
Zlata <alwaysssmile981@gmail.com>
2025-05-13 17:44:07 +0200

    Виправлено підключення стилів без Vite

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,8 +11,9 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <!-- Styles (заменили @vite на прямую ссылку на собранные ассеты) -->
+        <link rel="stylesheet" href="{{ asset('build/assets/app-DXnCgaXg.css') }}">
+        <script src="{{ asset('build/assets/app-Bo-u61x1.js') }}" defer></script>
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
