e153211e0519dd1137b3a408547a79f05afa55f5
Zlata <alwaysssmile981@gmail.com>
2025-05-14 10:04:44 +0200

    Виправлено посилання

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -12,10 +12,10 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Styles -->
-        <link rel="stylesheet" href="{{ asset('build/assets/app-DXnCgaXg.css') }}">
+        <link rel="stylesheet" href="{{ secure_asset('build/assets/app-DXnCgaXg.css') }}">
 
         <!-- Scripts -->
-        <script src="{{ asset('build/assets/app-Bo-u61x1.js') }}" defer></script>
+        <script src="{{ secure_asset('build/assets/app-Bo-u61x1.js') }}" defer></script>
     </head>
     <body class="font-sans text-gray-900 antialiased bg-gray-100">
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0">
